8396c09fd85372c97f81b4a94b6d5f005c0f892b
Oleg Pavlov <o.s.pavlov@yandex.ru>
2024-03-13 15:14:43 +0300

    The task is completed.

---
+++ src/main/java/org/example/order/Owner.java
@@ -0,0 +1,13 @@
+package org.example.order;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Owner {
+    private String name;
+    private String email;
+    private String createdAt;
+    private String updatedAt;
+}
